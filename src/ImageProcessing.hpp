8ab2088380dc26c899696277ce04223670ae6401
Filip Kočica <filda1997@gmail.com>
2018-10-17 12:10:51 +0200

    Formating

--- src/ImageProcessing.hpp
+++ src/ImageProcessing.hpp
@@ -25,22 +25,22 @@
  */
 namespace ImageProcessing
 {
-    /**
-     * @brief 3D rotation function
-     * @param [in] input Input matrix
-     * @param [out] output Output matrix
-     * @param [in] alpha Angle X axis
-     * @param [in] beta Angle Y axis
-     * @param [in] gamma Angle Z axis
-     * @param [in] dx Translation along the X axis
-     * @param [in] dy Translation along the Y axis
-     * @param [in] dz Translation along the Z axis
-     * @param [in] f Focal distance (distance between camera and image, a smaller number exaggerates the effect)
-     * Credits to: Michael Jepson (http://jepsonsblog.blogspot.com/2012/11/rotation-in-3d-using-opencvs.html)
-     */
-    void rotateImage(const cv::Mat &input, cv::Mat &output, double alpha, double beta, double gamma, double dx, double dy, double dz, double f);
+	/**
+	 * @brief 3D rotation function
+	 * @param [in] input Input matrix
+	 * @param [out] output Output matrix
+	 * @param [in] alpha Angle X axis
+	 * @param [in] beta Angle Y axis
+	 * @param [in] gamma Angle Z axis
+	 * @param [in] dx Translation along the X axis
+	 * @param [in] dy Translation along the Y axis
+	 * @param [in] dz Translation along the Z axis
+	 * @param [in] f Focal distance (distance between camera and image, a smaller number exaggerates the effect)
+	 * Credits to: Michael Jepson (http://jepsonsblog.blogspot.com/2012/11/rotation-in-3d-using-opencvs.html)
+	 */
+	void rotateImage(const cv::Mat &input, cv::Mat &output, double alpha, double beta, double gamma, double dx, double dy, double dz, double f);
 
-    /**
+	/**
 	 * @brief Resize copied image due to its position in background
 	 *
 	 * @param [in] m image to be copied
@@ -49,23 +49,23 @@ namespace ImageProcessing
 	 */
 	void resize(cv::Mat& m, int pos, int mid, int r);
 
-    /**
+	/**
 	 * @brief Copy image to specified location in background
-     * 
-     * @param [in, out] bg Background where image is copied
-     * @param [in] img Image copied to background
-     * @param [in] x Position X
-     * @param [in] y Position Y
-     * @param [in] b Should be luminescence modified ? (RNG)
-     * @param [in] v Luminescence (RNG)
+	 * 
+	 * @param [in, out] bg Background where image is copied
+	 * @param [in] img Image copied to background
+	 * @param [in] x Position X
+	 * @param [in] y Position Y
+	 * @param [in] b Should be luminescence modified ? (RNG)
+	 * @param [in] v Luminescence (RNG)
 	 */
 	void copy2bg(cv::Mat& bg, cv::Mat& img, cv::Mat& alpha, int& x, int& y, int& b, int& v);
 
 	/**
 	 * @brief Rotate image in Y axis
-     * 
-     * @param [in, out] img Rotated image
-     * @param [in] angle Angle
+	 * 
+	 * @param [in, out] img Rotated image
+	 * @param [in] angle Angle
 	 */
 	void rotateAngle(cv::Mat& img, double angle);
 }
